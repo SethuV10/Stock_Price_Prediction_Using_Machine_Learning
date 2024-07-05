3c941a22337d38edb4aea0a2e96a36a0086fc602
Alagammai Palaniyappan <plsethu10@gmail.com>
2024-07-05 07:26:25 +0530

    File Added

--- stockprice.py
+++ stockprice.py
@@ -26,3 +26,21 @@ df.shape
 df.describe()
 
 df.info()
+
+import matplotlib.pyplot as plt
+import pandas as pd 
+plt.figure(figsize=(15,5)) 
+df = pd.read_csv('C:\\Users\\alagu\\Downloads\\TSLA.csv')
+plt.plot(df['Close']) 
+plt.title('Tesla Close price.', fontsize=15) 
+plt.ylabel('Price in dollars.') 
+plt.show()
+
+import pandas as pd 
+df = pd.read_csv('C:\\Users\\alagu\\Downloads\\TSLA.csv')
+df.head()
+
+import pandas as pd 
+df = pd.read_csv('C:\\Users\\alagu\\Downloads\\TSLA.csv')
+df = df.drop(['Adj Close'], axis=1)
+df.isnull().sum()
